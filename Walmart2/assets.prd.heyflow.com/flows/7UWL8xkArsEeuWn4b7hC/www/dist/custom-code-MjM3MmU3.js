401c5f5d8d0bebc11cb177aa208a51dfd686aa4d
Stefanchen11 <chenxinyang96@gmail.com>
2025-07-25 14:54:07 +0800

    Delete Walmart2 directory

--- Walmart2/assets.prd.heyflow.com/flows/7UWL8xkArsEeuWn4b7hC/www/dist/custom-code-MjM3MmU3.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('heyflow-screen-view', (event) => {
-    // console.log('heyflow screen view:', event.detail);
-});
-
-window.addEventListener('heyflow-submit', (event) => {
-    // console.log('heyflow submit:', event.detail);
-});
